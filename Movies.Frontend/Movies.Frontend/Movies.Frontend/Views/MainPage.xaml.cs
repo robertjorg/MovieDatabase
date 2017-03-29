791e18ed1b337b2029837abac8d389e27b70ed01
Robert Jorgensen <Robert.jorgensen@healthcatalyst.com>
2017-03-28 22:43:29 -0600

    Getting Started

---
+++ Movies.Frontend/Movies.Frontend/Movies.Frontend/Views/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Movies.Frontend
+{
+    using Movies.Frontend.Views;
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GetAllMovies(object sender, EventArgs e)
+        {
+            this.Navigation.PushAsync(new GetAllMovies());
+        }
+
+        private void FindMovies(object sender, EventArgs e)
+        {
+            this.Navigation.PushAsync(new FindMovie());
+        }
+
+        private void AddMovies(object sender, EventArgs e)
+        {
+            this.Navigation.PushAsync(new AddMovies());
+        }
+    }
+}
